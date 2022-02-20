7f203bf5a6a63c4c728d0dba355902087f9bc786
ROve-295 <rove_295@yaah.net>
2022-02-21 00:49:01 +0800

    update

---
+++ src/main/java/Run.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+
+/**
+ * 启动项目
+ *
+ * @author CUI, Bingzhe
+ * @version 1.0
+ **/
+public class Run {
+    public static void main(String[] args) {
+        Application.launch(Main.class);
+    }
+}
